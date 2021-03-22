c085c325794fb66f650bd11dcbcb4f333bb266d8
Andrea Negri <andrea.negri4@studio.unibo.it>
2021-03-22 11:52:24 +0100

    Implementata classe ComplexImpl e il metodo Complex.apply

--- src/u04lab/code/Complex.scala
+++ src/u04lab/code/Complex.scala
@@ -8,7 +8,17 @@ trait Complex {
 }
 
 object Complex {
-  def apply(re:Double, im:Double):Complex = ??? // Fill here
+  def apply(re:Double, im:Double):Complex = new ComplexImpl(re, im)
+
+  private class ComplexImpl(override val re: Double, override val im: Double) extends Complex {
+    override def +(c: Complex): Complex = {
+      Complex((re + c.re), (im + c.im))
+    }
+
+    override def *(c: Complex): Complex = {
+      Complex((re * c.re - im * c.im), (re * c.im + im * c.re))
+    }
+  }
 }
 
 object TryComplex extends App {
