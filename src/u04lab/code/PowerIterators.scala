3722d35746d7095f7ae13245bb9280dd22b11b05
Andrea Negri <andrea.negri4@studio.unibo.it>
2021-03-23 15:42:43 +0100

    Iterator NON funzionante per correzione

--- src/u04lab/code/PowerIterators.scala
+++ src/u04lab/code/PowerIterators.scala
@@ -3,6 +3,8 @@ package u04lab.code
 import Optionals._
 import Lists._
 
+import scala.util.Random
+
 trait PowerIterator[A] {
   def next(): Option[A]
   def allSoFar(): List[A]
@@ -18,9 +20,44 @@ trait PowerIteratorsFactory {
 
 class PowerIteratorsFactoryImpl extends PowerIteratorsFactory {
 
-  override def incremental(start: Int, successive: Int => Int): PowerIterator[Int] = ???
+  import Streams._
 
-  override def fromList[A](list: List[A]): Unit = ???
+  def fromStream[A](stream: Stream[A]): PowerIterator[A] = {
 
-  override def randomBooleans(size: Int): PowerIterator[Boolean] = ???
+    class PowerIteratorImpl[A]() extends PowerIterator[A] {
+
+      import List._
+      import Streams.Stream._
+
+      private var elements: List[A] = nil
+      private var index: Int = 1
+
+      override def next(): Option[A] = {
+        val el = reverse(toList(take(stream)(index)))(0)
+        index += 1
+        elements = append(elements, List.Cons(el, Nil()))
+        Option.of(el)
+      }
+
+      override def allSoFar(): List[A] = {
+        elements
+      }
+
+      override def reversed(): PowerIterator[A] = {
+        fromStream(Stream.generate(reverse(toList(stream))))
+      }
+    }
+  }
+
+  override def incremental(start: Int, successive: Int => Int): PowerIterator[Int] = {
+    fromStream[Int](Stream.generate[Int](successive(start)))
+  }
+
+  override def fromList[A](list: List[A]): Unit = {
+    fromStream[A](Stream.generate(list))
+  }
+
+  override def randomBooleans(size: Int): PowerIterator[Boolean] = {
+    fromStream[Boolean](Stream.generate(Random.nextBoolean()))
+  }
 }
